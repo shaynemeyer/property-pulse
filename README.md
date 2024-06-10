3b33489989e64af8dcb141d955113401119d8f9a
Shayne Meyer <shayne.meyer@gmail.com>
2024-06-10 07:23:08 -0700

    added custom error page and loading page

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 A rental property demo application written in TypeScript and NextJS.
 
+---
+## What we used
+
+- [React Icons - Font Awesome 5](https://react-icons.github.io/react-icons/icons/fa/)
+- [React Spinners](https://github.com/davidhu2000/react-spinners)
--- app/properties/add/page.tsx
+++ app/properties/add/page.tsx
@@ -1,22 +1,4 @@
-import PropertyCard from "@/components/PropertyCard";
-import { properties } from "@/constants/properties";
-import { DiVim } from "react-icons/di";
-
 const AddPropertyPage = () => {
-  return (
-    <section className="px-4 py-6">
-      <div className="container-xl lg:container m-auto px-4 py-6">
-        {properties.length === 0 ? (
-          <p>No properties found.</p>
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {properties.map((property) => (
-              <PropertyCard key={property._id} property={property} />
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
-  );
+  return <div>AddPropertyPage</div>;
 };
 export default AddPropertyPage;
